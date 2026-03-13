6cc837c8663b71e55e1a9b971b9ba5736efc9513
Harry Bayliss <harry@hjb.dev>
2026-03-13 12:02:53 +0000

    inc ver

--- commands/help.go
+++ commands/help.go
@@ -7,7 +7,7 @@ import (
 
 func Help(notFoundError bool) {
 	theme.Title("pvm: PHP Version Manager")
-	theme.Info("Version 1.2.1")
+	theme.Info("Version 1.2.2")
 
 	if notFoundError {
 		theme.Error("Command not found")
